02f4ea4012579a9e7942e4002e50092b0df25aec
diplankeshwar <diplankeshwar@gmail.com>
2026-03-07 02:28:06 +0530

    Time: 0 ms (100%), Space: 10.1 MB (89.05%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+
+        int Max_loot_so_far = 0;
+        int Max_loot_2_house_back = 0;
+
+        for(int i=0 ; i<n ; i++){
+            int Curr_loot = max(Max_loot_so_far , Max_loot_2_house_back + nums[i]);
+
+            Max_loot_2_house_back = Max_loot_so_far;
+            Max_loot_so_far = Curr_loot;
+        }
+        return Max_loot_so_far;
+        
+    }
+};
