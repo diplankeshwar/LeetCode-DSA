f8c553fad4f8d58bb1a98c160eae079d53758d91
diplankeshwar <diplankeshwar@gmail.com>
2026-03-27 00:45:46 +0530

    Time: 0 ms (100%), Space: 23.8 MB (81.76%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n = nums.size();
+        int count = 0;
+
+
+        for(int i = 0 ; i<n ; i++){
+            if(nums[i]!=0){
+                nums[count] = nums[i];
+                count++;
+
+            }
+        }
+        while(count < nums.size()){
+            nums[count] = 0;
+            count++;
+        }
+
+    }
+};
