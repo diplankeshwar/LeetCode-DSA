c302c4df11e99ad2645e284d7251061e59065e18
diplankeshwar <diplankeshwar@gmail.com>
2026-09-23 22:28:32 +0530

    Time: 0 ms (100%), Space: 8.4 MB (86.76%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if (x < 2) return x;
+
+        long long low = 1;
+        long long high = x / 2;
+        int result = 1;
+
+        while (low <= high) {
+            long long mid = low + (high - low) / 2;
+
+            if (mid * mid <= x) {
+                result = mid;
+                low = mid + 1;
+            }
+            else {
+                high = mid - 1;
+            }
+        }
+
+        return result;
+    }
+};
